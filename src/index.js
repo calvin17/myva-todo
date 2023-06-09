7cd40fbe60c0cc8e3c7d75053d83f3bd4025de52
calvin.abraham7@gmail.com <calvin.abraham7@gmail.com>
2023-06-09 16:51:51 +0530

    updated the index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import('./bootstrap');
+// import('./bootstrap');
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard';
+import CreateTodo from './components/CreateTodo';
+
+export default () => {
+  return (
+    <div>
+      <Routes path="/">
+        <Route index element={<Dashboard />} />
+        <Route path=":new" element={<CreateTodo />} />
+      </Routes>
+    </div>
+  );
+};
