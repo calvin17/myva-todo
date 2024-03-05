5c8cb941b5217e4f09e35b5753592950bea3c197
calvin.abraham7@gmail.com <calvin.abraham7@gmail.com>
2024-03-06 02:49:55 +0530

    added firebase config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,14 +2,13 @@ const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
-const path = require('path');
 
 const prodConfig = {
   mode: 'production',
   entry: './src/App.tsx',
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: '/myva_todo/latest/',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
